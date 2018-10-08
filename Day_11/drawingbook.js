fb9bcb80bc2b704f4a9ee1cb03d3696133cad227
Harshil Gupta <harshil.g@tadigital.com>
2018-10-08 10:17:21 +0530

    Day 11 challenge drawing book added

---
+++ Day_11/drawingbook.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+function pageCount(n, p) {
+    /*
+     * Write your code here.
+     */
+    var begin = parseInt(p/2);
+         var total = parseInt(n/2);
+      
+        var end = total -begin;
+        if(end < begin){
+            return end;
+        }
+        else {
+            return begin;
+        }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const p = parseInt(readLine(), 10);
+
+    let result = pageCount(n, p);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
